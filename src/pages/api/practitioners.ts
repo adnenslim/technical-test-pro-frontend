19d47c926e4034490f55c0ef3161f6814aadb8e9
slim <adnen.slim.dev@gmail.com>
2022-01-27 15:14:56 +0100

    Feature 01: fix practitioner endpoint

--- src/pages/api/practitioners.ts
+++ src/pages/api/practitioners.ts
@@ -7,6 +7,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
       const practitioners = await prisma.practitioner.findMany();
 
       res.status(200).json(practitioners);
+      break;
 
     default:
       res.status(405).json({ error: `method ${req.method} Not Allowed` });
